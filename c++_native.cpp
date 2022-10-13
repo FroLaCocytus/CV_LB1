85c55193a4e0869b8b7f9a19e40f9cb705e68c53
Krivorotko Alexey <73778449+FroLaCocytus@users.noreply.github.com>
2022-10-13 23:45:00 +0300

    Add files via upload

--- c++_native.cpp
+++ c++_native.cpp
@@ -14,13 +14,30 @@ using namespace cv;
 using namespace std;
 
 
+void neighbors_update(Mat& array, int radius, int row_index, int column_index){
+
+    Vec3b pixelColor(255, 255, 255);
+
+    for (int j = column_index - radius; j < column_index + 1 + radius; j++) {
+        for (int i = row_index - radius; i < row_index + 1 + radius; i++) {
+            if (i >= 0 && i < array.rows && j >= 0 and j < array.cols) {
+                array.at<uchar>(i, j) = 255;
+            }
+        }
+    }
+}
+
+
+
 int main()
 {
+
     unsigned int start;
     unsigned int end;
     vector <float> test;
     ofstream excel;
     bool flag = true;
+
 
     Mat img_gray;
     Mat img_bin;
@@ -29,14 +46,27 @@ int main()
     Mat img = imread("space.jpg");
     cvtColor(img, img_gray, COLOR_BGR2GRAY);
     threshold(img_gray, img_bin, 127, 255, THRESH_BINARY);
-    
+
+
+
+
+
+
     for (int i = 0; i < 30; i++) {
         start = clock();
-        dilate(img_bin, img_dilate, Mat());
+
+        img_dilate = img_bin.clone();
+                      
+        for (int x = 0; x < img_bin.rows; x++) {
+            for (int y = 0; y < img_bin.cols; y++) {
+                if (img_bin.at<uchar>(x, y)==255) {
+                    neighbors_update(img_dilate, 1, x, y);
+                }
+            }
+        }
         end = clock();
         test.push_back((float)(end - start) / 1000);
     }
-
 
 
     while (1){
@@ -59,7 +89,6 @@ int main()
     }
 
     destroyAllWindows();
-
 
     excel.open("test.xls", fstream::app);
 
